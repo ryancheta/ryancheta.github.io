02f8466f9c5ed5dbde0ced9e514971822974f9fa
Ryan <ryan@Ryans-MacBook-Pro-15.local>
2019-03-14 17:41:17 -0700

    refactor hide-nav.js to scale

--- hide-nav.js
+++ hide-nav.js
@@ -1,11 +1,14 @@
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
+
   var currentScrollPos = window.pageYOffset;
+  var navHeight = (-1) * document.getElementsByClassName('nav')[0].offsetHeight;
+
   if (prevScrollpos > currentScrollPos) {
     document.getElementsByClassName("nav")[0].style.top = "0";
   } else {
-    document.getElementsByClassName("nav")[0].style.top = "-55px";
+    document.getElementsByClassName("nav")[0].style.top = navHeight + "px";
   }
   prevScrollpos = currentScrollPos;
 }
